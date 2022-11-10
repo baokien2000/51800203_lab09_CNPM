a89c7b5cdec40910efa51bb908c703ed207e0272
baokien2000 <baokien2000@gmail.com>
2022-11-10 08:26:26 +0700

    add GetLettertScore Testcase

--- ClassLibrary1/TestProject1/UnitTest1.cs
+++ ClassLibrary1/TestProject1/UnitTest1.cs
@@ -12,7 +12,34 @@ namespace TestProject1
             char Test = S1.getLetterScore();
             Assert.AreEqual(Test, 'A');
         }
-
+        [TestMethod]
+        public void getLetterScore_must_return_B()
+        {
+            StudentService.Student S1 = new Student() { Id = 1, Name = "Student", Age = 18, Score = 7.1 };
+            char Test = S1.getLetterScore();
+            Assert.AreEqual(Test, 'B');
+        }
+        [TestMethod]
+        public void getLetterScore_must_return_C()
+        {
+            StudentService.Student S1 = new Student() { Id = 1, Name = "Student", Age = 18, Score = 6.1 };
+            char Test = S1.getLetterScore();
+            Assert.AreEqual(Test, 'C');
+        }
+        [TestMethod]
+        public void getLetterScore_must_return_D()
+        {
+            StudentService.Student S1 = new Student() { Id = 1, Name = "Student", Age = 18, Score = 3.6 };
+            char Test = S1.getLetterScore();
+            Assert.AreEqual(Test, 'D');
+        }
+        [TestMethod]
+        public void getLetterScore_must_return_E()
+        {
+            StudentService.Student S1 = new Student() { Id = 1, Name = "Student", Age = 18, Score = 0 };
+            char Test = S1.getLetterScore();
+            Assert.AreEqual(Test, 'E');
+        }
 
     }
 }
